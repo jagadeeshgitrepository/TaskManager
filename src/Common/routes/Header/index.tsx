44e1ffd30d5e3f6d57cdface38d2b38df2c939a2
jagadeesh <jagadeeshnoddy12@gmail.com>
2022-05-16 16:25:42 +0530

    added header

--- src/Common/routes/Header/index.tsx
+++ src/Common/routes/Header/index.tsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
-import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
+import Dropdown from 'react-dropdown'
+import './header.css'
 import {
    TaskManagerHeader,
    TaskManagerHeaderMenuItems,
    TaskManagerMenuItemList,
    TaskManagerHeaderHome,
-   TaskManagerDropDown
+   TaskManagerHeaderBoards,
+   TaskManagerHeaderBoardLogo,
+   TaskManagerHeaderBoardHeading,
+   TaskManagerHeaderLogo,
+   TaskManagerSearchContainer,
+   TaskManagerSearchInput,
+   TaskManagerLogoutHeading,
+   TaskManagerProfile,
+   TaskManagerProfileHeading
 } from './style'
+
+import 'react-dropdown/style.css'
 
 class Header extends Component {
    render(): React.ReactElement {
@@ -17,11 +29,34 @@ class Header extends Component {
                <TaskManagerMenuItemList>
                   <TaskManagerHeaderHome>
                      <AiOutlineHome />
-                     <TaskManagerDropDown
-                        options={options}
-                        placeholder='Select an option'
-                     />
                   </TaskManagerHeaderHome>
+                  <Dropdown
+                     options={options}
+                     placeholder='Organization'
+                     className='w-40 h-10 bg-sky-500'
+                  />
+                  <TaskManagerHeaderBoards>
+                     <TaskManagerHeaderBoardLogo src='https://res.cloudinary.com/dmpepn8dm/image/upload/v1652693709/Task%20Manager/logo_xdfebn.png' />
+                     <TaskManagerHeaderBoardHeading>
+                        Boards
+                     </TaskManagerHeaderBoardHeading>
+                  </TaskManagerHeaderBoards>
+               </TaskManagerMenuItemList>
+               <TaskManagerMenuItemList>
+                  <TaskManagerHeaderLogo src='https://res.cloudinary.com/dmpepn8dm/image/upload/v1652694609/Task%20Manager/Group_7401_3_spui3s.png' />
+               </TaskManagerMenuItemList>
+               <TaskManagerMenuItemList>
+                  <TaskManagerSearchContainer>
+                     <TaskManagerSearchInput
+                        type='search'
+                        placeholder='search'
+                     />
+                     <AiOutlineSearch />
+                  </TaskManagerSearchContainer>
+                  <TaskManagerLogoutHeading>Log Out</TaskManagerLogoutHeading>
+                  <TaskManagerProfile>
+                     <TaskManagerProfileHeading>WJ</TaskManagerProfileHeading>
+                  </TaskManagerProfile>
                </TaskManagerMenuItemList>
             </TaskManagerHeaderMenuItems>
          </TaskManagerHeader>
--- src/Common/routes/Header/style.tsx
+++ src/Common/routes/Header/style.tsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components'
-import Dropdown from 'react-dropdown'
-
 import tw from 'twin.macro'
 
 export const TaskManagerHeader = styled.nav`
@@ -10,17 +8,61 @@ export const TaskManagerHeader = styled.nav`
 `
 
 export const TaskManagerHeaderMenuItems = styled.ul`
-   ${tw`flex flex-row items-center list-none justify-between `};
+   ${tw`flex flex-row items-center w-screen justify-between list-none pl-0`};
 `
 
-export const TaskManagerMenuItemList = styled.li``
+export const TaskManagerMenuItemList = styled.li`
+   ${tw`flex flex-row items-center   justify-between w-96`};
+`
 
 export const TaskManagerHeaderHome = styled.div`
    ${tw`w-10 h-10 text-white flex flex-row items-center  justify-center `};
    background-color: #4e97c2;
    border-radius: 4px;
 `
-export const TaskManagerDropDown = styled.DropDown`
-   ${tw`w-40 h-10`};
-   background-color: '#4E97C2';
+
+export const TaskManagerHeaderBoards = styled.div`
+   ${tw` h-10 text-white flex flex-row items-center `};
+   width: 136px;
+   justify-content: space-around;
+   background-color: #2fa9f1;
+   border-radius: 4px;
+`
+
+export const TaskManagerHeaderBoardLogo = styled.img`
+   ${tw`w-6 h-6`};
+`
+
+export const TaskManagerHeaderBoardHeading = styled.h1`
+   ${tw`font-bold	`}
+`
+export const TaskManagerHeaderLogo = styled.img`
+   ${tw`w-48 h-10`}
+`
+
+export const TaskManagerSearchContainer = styled.div`
+   ${tw`flex flex-row items-center h-10  text-white`}
+   justify-content:space-around;
+   width: 264px;
+   background-color: #4e97c2;
+   border-radius: 4px;
+`
+
+export const TaskManagerSearchInput = styled.input`
+   ${tw`bg-transparent w-48 border-0 `};
+   outline: none;
+`
+
+export const TaskManagerLogoutHeading = styled.h1`
+   ${tw`text-white`};
+`
+export const TaskManagerProfile = styled.div`
+   ${tw`text-white w-10 h-10 rounded-3xl`};
+   background-color: #bae3ff;
+`
+
+export const TaskManagerProfileHeading = styled.h1`
+   ${tw`flex flex-col justify-center m-2 items-center`};
+   background-color: #bae3ff;
+   color: #0967d2;
 `
